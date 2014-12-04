6e920b1caa4665a30f1919df2c5a5cddd1644d4a
Alfredo Varela <Alfredo Varela@Alfredo>
2014-12-04 19:22:28 +0100

    haciendo el primer get

--- blackmarket-api/src/main/java/edu/upc/eetac/dsa/dsaqt2014g1/blackmarket/api/BlackResource.java
+++ blackmarket-api/src/main/java/edu/upc/eetac/dsa/dsaqt2014g1/blackmarket/api/BlackResource.java
@@ -1,5 +1,119 @@
 package edu.upc.eetac.dsa.dsaqt2014g1.blackmarket.api;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import javax.sql.DataSource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import edu.upc.eetac.dsa.dsaqt2014g1.blackmarket.api.model.Black;
+import edu.upc.eetac.dsa.dsaqt2014g1.blackmarket.api.model.BlackCollection;
+
+
+
+
+@Path("/blacks")
 public class BlackResource {
+	
+	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
+	
+	private String GET_BLACK_QUERY_ASIGNATURA = "SELECT * FROM contenidos id_asignatura=? and id_tipo=?";
+	private String GET_BLACK_QUERY_MATRICULADAS = "select * from contenidos, users_matriculas where username_matriculas=? and id_asignatura_u_matriculas=id_asignatura";
+	private String GET_BLACK_QUERY_TITULO = "SELECT * FROM contenidos where titulo=?";
+	private String GET_BLACK_QUERY_AUTOR_FROM_LAST = "select c.* from contenidos c where autor LIKE ? and c.fecha > ? order by fecha";
+	private String GET_BLACKS_QUERY_AUTOR = "select c.* from contenidos c where autor LIKE ? and c.fecha < ifnull ( ?, now()) order by fecha desc limit ?";
+	private String GET_BLACK_QUERY_USUARIO = "SELECT * FROM contenidos where id_usuario=?";
+	//@Context
+	//private SecurityContext security;
+	
+	@GET
+	@Path("/search/{nombreautor}")
+	@Produces(MediaType.BLACKS_API_BLACK_COLLECTION)
+	public BlackCollection getBlack(
+			@PathParam("nombreautor") String nombreautor,
+			@QueryParam("length") int length,
+			@QueryParam("before") long before, @QueryParam("after") long after) {
+		BlackCollection coleccionblack = new BlackCollection();
+		coleccionblack.setPattern(nombreautor);
+
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+		PreparedStatement stmt = null;
+		try {
+			boolean updateFromLast = after > 0;
+			stmt = updateFromLast ? conn
+					.prepareStatement(GET_BLACK_QUERY_AUTOR_FROM_LAST ) : conn
+					.prepareStatement(GET_BLACKS_QUERY_AUTOR);
+			stmt.setString(1, '%' + nombreautor + '%');
+
+			if (updateFromLast) {
+				stmt.setTimestamp(2, new Timestamp(after));
+
+			} else {
+				if (before > 0) {
+					stmt.setTimestamp(2, new Timestamp(before));
+
+				} else
+					stmt.setTimestamp(2, null);
+				length = (length <= 0) ? 5 : length;
+				stmt.setInt(3, length);
+			}
+
+			ResultSet rs = stmt.executeQuery();
+
+			boolean first = true;
+			long oldestTimestamp = 0;
+			while (rs.next()) {
+				Black black = new Black();
+				black.setId_contenido(rs.getInt("id_contenido"));
+				black.setId_asignatura(rs.getInt("id_asignatura"));
+				black.setId_tipo(rs.getInt("id_tipo"));
+				black.setTitulo(rs.getString("titulo"));
+				black.setDescripcion(rs.getString("descripcion"));
+				black.setAutor(rs.getString("autor"));
+				black.setLink(rs.getString("link"));
+				black.setInvalid(rs.getInt("invalid"));
+				black.setFecha(rs.getTimestamp("fecha").getTime());
+				oldestTimestamp = rs.getTimestamp("fecha").getTime();
+				black.setFecha(oldestTimestamp);
+				if (first) {
+					first = false;
+					coleccionblack.setNewestTimestamp(black.getFecha());
+				}
+				coleccionblack.addBlack(black);
+			}
+			coleccionblack.setOldestTimestamp(oldestTimestamp);
+
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+
+		return coleccionblack;
+	}
+	
 
 }
--- blackmarket-api/src/main/java/edu/upc/eetac/dsa/dsaqt2014g1/blackmarket/api/model/BlackCollection.java
+++ blackmarket-api/src/main/java/edu/upc/eetac/dsa/dsaqt2014g1/blackmarket/api/model/BlackCollection.java
@@ -1,5 +1,6 @@
 package edu.upc.eetac.dsa.dsaqt2014g1.blackmarket.api.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.core.Link;
@@ -11,27 +12,38 @@ import org.glassfish.jersey.linking.InjectLink.Style;
 
 
 
+
+
 public class BlackCollection {
 
 	//@InjectLinks({
 		//@InjectLink(resource = StingResource.class, style = Style.ABSOLUTE, rel = "create-sting", title = "Create sting", type = MediaType.BEETER_API_STING),
 		//@InjectLink(value = "/stings?before={before}", style = Style.ABSOLUTE, rel = "previous", title = "Previous stings", type = MediaType.BEETER_API_STING_COLLECTION, bindings = { @Binding(name = "before", value = "${instance.oldestTimestamp}") }),
 		//@InjectLink(value = "/stings?after={after}", style = Style.ABSOLUTE, rel = "current", title = "Newest stings", type = MediaType.BEETER_API_STING_COLLECTION, bindings = { @Binding(name = "after", value = "${instance.newestTimestamp}") }) })
+private String pattern;
 private List<Link> links;
-private List<Black> stings;
+private List<Black> blacks;
 private long newestTimestamp;
 private long oldestTimestamp;
 public List<Link> getLinks() {
 	return links;
 }
+public BlackCollection(){
+	super();
+	blacks = new ArrayList<>();
+}
+public void addBlack(Black black){
+	blacks.add(black);
+}
+
 public void setLinks(List<Link> links) {
 	this.links = links;
 }
-public List<Black> getStings() {
-	return stings;
+public List<Black> getBlacks() {
+	return blacks;
 }
-public void setStings(List<Black> stings) {
-	this.stings = stings;
+public void setBlack(List<Black> black) {
+	this.blacks = black;
 }
 public long getNewestTimestamp() {
 	return newestTimestamp;
@@ -45,7 +57,13 @@ public long getOldestTimestamp() {
 public void setOldestTimestamp(long oldestTimestamp) {
 	this.oldestTimestamp = oldestTimestamp;
 }
+public String getPattern() {
+	return pattern;
+}
 
+public void setPattern(String pattern) {
+	this.pattern = pattern;
+}
 
 
 

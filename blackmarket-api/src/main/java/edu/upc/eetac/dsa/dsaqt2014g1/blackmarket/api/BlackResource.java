b8c15cc6c02d914cd36e9ec13afb314cf76975a8
pc <pc@Miranda>
2014-12-06 23:19:11 +0100

    GET todos "prueba"

--- blackmarket-api/src/main/java/edu/upc/eetac/dsa/dsaqt2014g1/blackmarket/api/BlackResource.java
+++ blackmarket-api/src/main/java/edu/upc/eetac/dsa/dsaqt2014g1/blackmarket/api/BlackResource.java
@@ -8,19 +8,21 @@ import java.sql.Timestamp;
 
 import javax.sql.DataSource;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 import edu.upc.eetac.dsa.dsaqt2014g1.blackmarket.api.model.Black;
 import edu.upc.eetac.dsa.dsaqt2014g1.blackmarket.api.model.BlackCollection;
-
-
 
 
 @Path("/blacks")
@@ -29,14 +31,60 @@ public class BlackResource {
 	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
 	
 	private String GET_BLACK_QUERY_ASIGNATURA = "SELECT * FROM contenidos id_asignatura=? and id_tipo=?";
+	//He añadido esta 
+	private String GET_BLACK_QUERY_CONTENIDO = "SELECT * FROM contenidos";
 	private String GET_BLACK_QUERY_MATRICULADAS = "select * from contenidos, users_matriculas where username_matriculas=? and id_asignatura_u_matriculas=id_asignatura";
 	private String GET_BLACK_QUERY_TITULO = "SELECT * FROM contenidos where titulo=?";
 	private String GET_BLACK_QUERY_AUTOR_FROM_LAST = "select c.* from contenidos c where autor LIKE ? and c.fecha > ? order by fecha";
 	private String GET_BLACKS_QUERY_AUTOR = "select c.* from contenidos c where autor LIKE ? and c.fecha < ifnull ( ?, now()) order by fecha desc limit ?";
 	private String GET_BLACK_QUERY_USUARIO = "SELECT * FROM contenidos where id_usuario=?";
+	
 	//@Context
 	//private SecurityContext security;
 	
+	@GET
+	@Produces(MediaType.BLACKS_API_BLACK_COLLECTION)
+	public BlackCollection getBlack() {
+		BlackCollection blacks = new BlackCollection();
+	 
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+	 
+		PreparedStatement stmt = null;
+		try {
+			stmt = conn.prepareStatement(GET_BLACK_QUERY_CONTENIDO);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Black black = new Black();
+				black.setId_contenido(rs.getInt("id_contenido"));
+				/*black.setId_asignatura(rs.getInt("id_asignatura"));
+				black.setId_tipo(rs.getInt("id_tipo"));
+				black.setTitulo(rs.getString("titulo"));
+				black.setDescripcion(rs.getString("descripcion"));
+				black.setAutor(rs.getString("autor"));
+				black.setInvalid(rs.getInt("invalid"));*/
+				blacks.addBlack(black);
+			}
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+		return blacks;
+	}
+	
+
 	@GET
 	@Path("/search/{nombreautor}")
 	@Produces(MediaType.BLACKS_API_BLACK_COLLECTION)
@@ -114,6 +162,4 @@ public class BlackResource {
 
 		return coleccionblack;
 	}
-	
-
 }

4d5741c05bb707c593c8eb2f1cf7cf64a5cee20f
Alfredo Varela <Alfredo Varela@Alfredo>
2015-01-03 11:01:40 +0100

    el post de fotos funciona man

--- blackmarket-api/src/main/java/edu/upc/eetac/dsa/dsaqt2014g1/blackmarket/api/BlackApplication.java
+++ blackmarket-api/src/main/java/edu/upc/eetac/dsa/dsaqt2014g1/blackmarket/api/BlackApplication.java
@@ -4,19 +4,25 @@ import java.util.Enumeration;
 import java.util.ResourceBundle;
 
 import org.glassfish.jersey.linking.DeclarativeLinkingFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class BlackApplication  extends ResourceConfig {
 
 	public BlackApplication() {
 		super();
-		register(DeclarativeLinkingFeature.class);
+		//register(DeclarativeLinkingFeature.class);
+		//ResourceBundle bundle = ResourceBundle.getBundle("application");
+		
+		register(MultiPartFeature.class);
 		ResourceBundle bundle = ResourceBundle.getBundle("application");
 
 		Enumeration<String> keys = bundle.getKeys();
 		while (keys.hasMoreElements()) {
 			String key = keys.nextElement();
 			property(key, bundle.getObject(key));
+			
+			
 		}
 	}
 }
